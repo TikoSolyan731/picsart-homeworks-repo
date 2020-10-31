9355d5d3c499171fb084c955e7969dc3e2060039
Tiko <tikosolyan731@gmail.com>
2020-10-31 13:00:32 +0400

    Added Homework 1

---
+++ HomeWork 1/src/Homework1.java
@@ -0,0 +1,24 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 245;
+        int x = 123;
+        long l = 1234352353123L;
+        char ch = '\u0531';
+        boolean isDone = true;
+        float m = 21.254F;
+        double n = 15.486;
+
+        String text = "Hello World!";
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of l: " + l);
+        System.out.println("Value of ch: " + ch);
+        System.out.println("Value of isDone: " + isDone);
+        System.out.println("Value of m: " + m);
+        System.out.println("Value of n: " + n);
+        System.out.println("Value of text: " + text);
+    }
+}
